9a4b64ae31e66672fd304ed8df51b657cc0393a3
JoaquinCorcheroFMP <joaquin.corchero@gmail.com>
2016-09-05 09:02:58 +0100

    Added the user controller and the routes (p44)
    Signed-off-by: JoaquinCorcheroFMP <joaquin.corchero@gmail.com>

--- rumbl/lib/rumbl/repo.ex
+++ rumbl/lib/rumbl/repo.ex
@@ -5,9 +5,9 @@ defmodule Rumbl.Repo do
   """
 
   def all(Rumbl.User) do
-    [%Rumbl.User{id: 1, name="Pepe", username="pepe.gotera" password="password1"},
-    %Rumbl.User{id: 2, name="Antonio", username="antonio.gotera" password="password2"},
-    %Rumbl.User{id: 3, name="Juan", username="juan.gotera" password="password3"}]
+    [%Rumbl.User{id: 1, name: "Pepe", username: "pepe.gotera", password: "password1"},
+    %Rumbl.User{id: 2, name: "Antonio", username: "antonio.gotera", password: "password2"},
+    %Rumbl.User{id: 3, name: "Juan", username: "juan.gotera", password: "password3"}]
   end
 
   def all(module), do: []
@@ -19,6 +19,7 @@ defmodule Rumbl.Repo do
   def get_by(module, params) do
     Enum.find all(module), fn map ->
       Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end
   end
 
 end
