37c91e04f06c7ad2d9332d4905f27a1642d55356
JoaquinCorcheroFMP <joaquin.corchero@gmail.com>
2016-09-05 09:26:46 +0100

    Implemented the view for a single user (p48)
    Signed-off-by: JoaquinCorcheroFMP <joaquin.corchero@gmail.com>

--- rumbl/lib/rumbl/repo.ex
+++ rumbl/lib/rumbl/repo.ex
@@ -4,9 +4,9 @@ defmodule Rumbl.Repo do
   """
 
   def all(Rumbl.User) do
-    [%Rumbl.User{id: 1, name: "Pepe", username: "pepe.gotera", password: "password1"},
-    %Rumbl.User{id: 2, name: "Antonio", username: "antonio.gotera", password: "password2"},
-    %Rumbl.User{id: 3, name: "Juan", username: "juan.gotera", password: "password3"}]
+    [%Rumbl.User{id: "1", name: "Pepe", username: "pepe.gotera", password: "password1"},
+    %Rumbl.User{id: "2", name: "Antonio", username: "antonio.gotera", password: "password2"},
+    %Rumbl.User{id: "3", name: "Juan", username: "juan.gotera", password: "password3"}]
   end
 
   def all(module), do: []
