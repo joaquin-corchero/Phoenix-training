3a46400840c3d7daac187cc87cf4d4eaa457b1f5
JoaquinCorcheroFMP <joaquin.corchero@gmail.com>
2016-09-06 09:17:48 +0100

    Added migrations and the some data seeding (p58)
    Signed-off-by: JoaquinCorcheroFMP <joaquin.corchero@gmail.com>

--- rumbl/lib/rumbl/repo.ex
+++ rumbl/lib/rumbl/repo.ex
@@ -1,8 +1,12 @@
 defmodule Rumbl.Repo do
   use Ecto.Repo, otp_app: :rumbl
 
-    #[%Rumbl.User{id: "1", name: "Pepe", username: "pepe.gotera", password: "password1"},
-    #%Rumbl.User{id: "2", name: "Antonio", username: "antonio.gotera", password: "password2"},
-    #%Rumbl.User{id: "3", name: "Juan", username: "juan.gotera", password: "password3"}]
+#To seed the db from console:
+#iex -S mix
+#alias Rumbl.Repo
+#alias Rumbl.User
+#Repo.insert(%User{name: "Pepe", username: "pepe.gotera", pasword_hash: "password1"})
+#Repo.insert(%User{name: "Antonio", username: "antonio.gotera", pasword_hash: "password2"})
+#Repo.insert(%User{name: "Juan", username: "juan.gotera", pasword_hash: "password3"})
   
 end
