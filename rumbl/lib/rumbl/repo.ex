b0d403050d865cb0babe539c5d66a45b1dd6b91c
JoaquinCorcheroFMP <joaquin.corchero@gmail.com>
2016-09-06 08:32:43 +0100

    Preparation for Ecto (p54)
    Signed-off-by: JoaquinCorcheroFMP <joaquin.corchero@gmail.com>

--- rumbl/lib/rumbl/repo.ex
+++ rumbl/lib/rumbl/repo.ex
@@ -1,24 +1,9 @@
 defmodule Rumbl.Repo do
-  @moduledoc """
-  In memory repo
-  """
+  use Ecto.Repo, otp_app: :rumbl
 
   def all(Rumbl.User) do
-    [%Rumbl.User{id: "1", name: "Pepe", username: "pepe.gotera", password: "password1"},
-    %Rumbl.User{id: "2", name: "Antonio", username: "antonio.gotera", password: "password2"},
-    %Rumbl.User{id: "3", name: "Juan", username: "juan.gotera", password: "password3"}]
-  end
-
-  def all(module), do: []
-
-  def get(module, id) do
-    Enum.find all(module), fn map -> map.id == id end
-  end
-
-  def get_by(module, params) do
-    Enum.find all(module), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end
-  end
-
+    #[%Rumbl.User{id: "1", name: "Pepe", username: "pepe.gotera", password: "password1"},
+    #%Rumbl.User{id: "2", name: "Antonio", username: "antonio.gotera", password: "password2"},
+    #%Rumbl.User{id: "3", name: "Juan", username: "juan.gotera", password: "password3"}]
+  
 end
