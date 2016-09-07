8751203ead213af5fc9333ef0cbf519717269ab2
JoaquinCorcheroFMP <joaquin.corchero@gmail.com>
2016-09-07 08:48:36 +0100

    Implemented changeset for the model, action on controller and create form (p64)
    Signed-off-by: JoaquinCorcheroFMP <joaquin.corchero@gmail.com>

--- rumbl/web/models/user.ex
+++ rumbl/web/models/user.ex
@@ -1,6 +1,6 @@
 defmodule Rumbl.User do
   use Rumbl.Web, :model
-  #to create the migration you need to execute 
+  #to create the migration you need to execute
   #mix phoenix.gen.html User users name:string username:string pasword_hash:string
   schema "users" do
     field :name, :string
@@ -14,9 +14,9 @@ defmodule Rumbl.User do
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
-  def changeset(struct, params \\ %{}) do
-    struct
-    |> cast(params, [:name, :username, :pasword_hash])
-    |> validate_required([:name, :username, :pasword_hash])
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, ~w(name username), [])
+    |> validate_length(:username, min: 1, max: 20)
   end
 end
