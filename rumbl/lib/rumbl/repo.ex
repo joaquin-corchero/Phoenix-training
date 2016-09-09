1aae7531395cb13c1556419406bfa4f683f8d877
JoaquinCorcheroFMP <joaquin.corchero@gmail.com>
2016-09-09 08:54:27 +0100

    Added the seeding to hash all the passwords (p73)
    Signed-off-by: JoaquinCorcheroFMP <joaquin.corchero@gmail.com>

--- rumbl/lib/rumbl/repo.ex
+++ rumbl/lib/rumbl/repo.ex
@@ -1,12 +1,4 @@
 defmodule Rumbl.Repo do
   use Ecto.Repo, otp_app: :rumbl
 
-#To seed the db from console:
-#iex -S mix
-#alias Rumbl.Repo
-#alias Rumbl.User
-#Repo.insert(%User{name: "Pepe", username: "pepe.gotera", pasword_hash: "password1"})
-#Repo.insert(%User{name: "Antonio", username: "antonio.gotera", pasword_hash: "password2"})
-#Repo.insert(%User{name: "Juan", username: "juan.gotera", pasword_hash: "password3"})
-  
 end
--- rumbl/priv/repo/seeds.exs
+++ rumbl/priv/repo/seeds.exs
@@ -15,3 +15,7 @@ alias Rumbl.User
 Repo.insert!(%User{name: "Pepe", username: "pepe.gotera", password_hash: "password1"})
 Repo.insert!(%User{name: "Antonio", username: "antonio.gotera", password_hash: "password2"})
 Repo.insert!(%User{name: "Juan", username: "juan.gotera", password_hash: "password3"})
+
+for(u <- Rumbl.Repo.all(User)) do
+  Rumbl.Repo.update!(User.registration_changeset(u, %{password: u.password_hash || "temppass"}))
+end
